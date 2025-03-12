78da40c89c0a1ba7fac1711bf6ac8e4abf38a5eb
JaviGarcia <celmerjavier72@hotmail.com>
2025-03-11 21:30:21 -0600

    Users Already Can Register

--- UsersManagementsEmployees.Web/Program.cs
+++ UsersManagementsEmployees.Web/Program.cs
@@ -1,8 +1,28 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using UsersManagementsEmployees.Web;
+using MudBlazor.Services;
+using Management;
+using Refit;
+
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+
+ApiSetting apiSettings = builder.Configuration.GetSection(nameof(ApiSetting))
+                                                     .Get<ApiSetting>()!;
+
+builder.Services.AddRefitClient<IEmployeeSdk>()
+    .ConfigureHttpClient(c => c.BaseAddress = new Uri(apiSettings.UrlManagementApi));
+
+builder.Services.AddRefitClient<IUserSdk>()
+    .ConfigureHttpClient(c => c.BaseAddress = new Uri(apiSettings.UrlManagementApi));
+
+
+
+builder.Services.AddMudServices();
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
