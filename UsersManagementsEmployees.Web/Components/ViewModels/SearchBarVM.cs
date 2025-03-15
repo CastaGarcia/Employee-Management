96d75e4990cbc70ff6849c992076fe432a809d8c
JaviGarcia <celmerjavier72@hotmail.com>
2025-03-14 21:58:33 -0600

    SearchBar with Rendering in same page fromQry

--- UsersManagementsEmployees.Web/Components/ViewModels/SearchBarVM.cs
+++ UsersManagementsEmployees.Web/Components/ViewModels/SearchBarVM.cs
@@ -1,6 +1,13 @@
-﻿namespace UsersManagementsEmployees.Web.Components.ViewModels
+﻿using Management.Inputs;
+using Management;
+
+namespace UsersManagementsEmployees.Web.Components.ViewModels
 {
     public class SearchBarVM
     {
+        public string? NameContains { get; set; }        
     }
+
+    
+
 }
